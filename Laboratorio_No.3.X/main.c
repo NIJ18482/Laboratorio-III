4d7816235b6befef7123a438ccbb6263ecb60300
NIJ18482 <nij18482@uvg.edu.gt>
2020-02-08 23:07:15 -0600

    Palabra de Configuración
    Protocolo de palabra de configuración estableciendo entre otras cosas, tipo de reloj del pic.

---
+++ Laboratorio_No.3.X/main.c
@@ -0,0 +1,20 @@
+ /************************** PALABRA DE CONFIGURACION *************************/
+    #pragma config FOSC = INTRC_NOCLKOUT           
+    #pragma config WDTE = OFF       
+    #pragma config PWRTE = OFF      
+    #pragma config MCLRE = OFF      
+    #pragma config CP = OFF         
+    #pragma config CPD = OFF         
+    #pragma config BOREN = OFF     
+    #pragma config IESO = OFF      
+    #pragma config FCMEN = OFF     
+    #pragma config LVP = OFF       
+    #pragma config BOR4V = BOR40V   
+    #pragma config WRT = OFF        
+    #include <xc.h>
+    #include <stdint.h>
+   /***************************************************************************/
+
+void main(void) {
+    return;
+}
---
+++ Laboratorio_No.3.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Laboratorio_No.3.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Laboratorio_No.3.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=laboratoriono.3.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/laboratoriono.3.x.tar
